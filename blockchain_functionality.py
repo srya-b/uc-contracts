ad8f61478ffa3a01e25f1a804fc4723d734854b5
Surya Bakshi <sbaks0820@gmail.com>
2018-11-09 12:11:20 -0600

    commented the blockchain functionality to explain implementation, design choices and TODOs

--- blockchain_functionality.py
+++ blockchain_functionality.py
@@ -2,7 +2,10 @@ import gevent
 from gevent.event import AsyncResult
 from gevent.queue import Queue, Channel
 
-
+# Example definition of a contract, the object can be parameterized and it
+# returns a callable object that represents the contract functionality, 
+# this design was chosen so that the contract function could handle
+# holding all state information and persistent storage of the contract.
 def contract_addition():
     def _func(g, a, b):
         return int(a)+int(b)
@@ -16,7 +19,10 @@ def contract_input(N):
             _inputs[pid].set(inp)
     return _func
 
-
+# Global functions is an object passed into each contract function when
+# it is called. It's meant to mimic the globally available variables
+# that contracts are supposed to have like the current block number and
+# the block hash, the balances of addresses, etc...
 class GlobalFunctions(object):
     def __init__(self, blocks, balances, current_block):
         self._blocks = blocks
@@ -28,7 +34,16 @@ class GlobalFunctions(object):
    
     def balance(self, a):
         return self._balances[a]
-    
+
+# The main blockchain functionality that is parameterized by some set of 
+# contracts. The contracts are callable and transactions to them are
+# buffered until someone mines a new block and triggers processing of all
+# pending transactions. The ledger tracks the blocks, transactions, 
+# events (somewhat, yes) and the transfer of balances.
+# TODO: it can only return values to parties that called the contract, so
+# a functionality to add is the ability to consume events from the chain.
+# This could just be as simple as a buffer where any party consumes events
+# until it finds some that it cares about.
 class Ledger_Functionality(object):
     def __init__(self, sid, contracts, N, delta=2, block_reward=10):
         self.sid = sid
@@ -50,7 +65,11 @@ class Ledger_Functionality(object):
         caller,addr,args = tx
         result = self.contracts[addr](g, *args)
         self.outputs[caller].put(result)
- 
+
+    # Processes the transactions in the current buffer when a new
+    # block is triggered to be mined when 'tick' is sent to the 
+    # functionality. Mining also pays out the beneficiary specified
+    # with the command. 
     def new_block(self, beneficiary):
         self.current_block += 1
         new_block = (self.current_block, self.buffer_txs)
@@ -60,7 +79,8 @@ class Ledger_Functionality(object):
             self.process_tx(tx)
 
         self.buffer_txs = []
-
+    # Continuously running while loop which waits for input transactions
+    # and triggers to mine new blocks.
     def run(self):
         while True:
             caller,addr,args = self.input.get()
@@ -70,6 +90,8 @@ class Ledger_Functionality(object):
             else:
                 self.buffer_txs.append((caller,addr,args))
 
+# The ideal protocol of the blockchain where users trigger the functionality
+# with inputs such as transcations (contract calls only atm) and new blocks.
 def Blockchain_IdealProtocol(N):
     class Ledger_IdealProtocol(object):
         _instances = {}
@@ -85,6 +107,8 @@ def Blockchain_IdealProtocol(N):
 
     return Ledger_IdealProtocol
 
+# Test function where parties give inputs to different contracts then
+# tell the blockchain to mine a new block and prcess them.
 def give_inputs(parties):
     for i in range(len(parties)):
         print('placing input party', i)
@@ -110,7 +134,6 @@ def give_inputs(parties):
         result = parties[i].output.get()
         print(i,'+', i+1, '=', result) 
 
-# for now test sequential transactions all processed serially
 def test_eth_protocol_simple(N):
     contracts = [contract_addition(), contract_input(N)]
     ETH = Blockchain_IdealProtocol(N)
@@ -136,6 +159,8 @@ def test_eth_protocol_simple_transfer(N):
 
     #gevent.joinall([functionality, inputs])
 
+    # Ensure that the main process is alive so that the
+    # greenlets don't stop and blockchain can keep running
     while True:
         gevent.sleep()
 if __name__=='__main__':
