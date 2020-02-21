a358ff8ca09c0105eadda77589354dbc4b5eeefc
Surya Bakshi <sbaks0820@gmail.com>
2020-02-21 10:59:33 -0600

    stripped dummy adversary down

--- src/adversary.py
+++ src/adversary.py
@@ -26,44 +26,6 @@ class DummyAdversary(object):
     def read(self, fro, msg):
         print(u'{:>20} -----> {}, msg={}'.format(str(fro), str(self), msg))
 
-    def addParty(self, itm):
-        if (itm.sid,itm.pid) not in self.parties:
-            self.parties[itm.sid,itm.pid] = itm
-
-    def addParties(self, itms):
-        for itm in itms:
-            self.addParty(itm)
-
-    def partyInput(self, to, msg):
-        self.F.input_msg(('party-input', to, msg))
-
-    def input_delay_tx(self, fro, nonce, rounds):
-        msg = ('delay-tx', fro, nonce, rounds)
-        self.a2f.write( ((69,'G_ledger'), (False, msg)) )
-        r = gevent.wait(objects=[self.f2a],count=1)
-        r = r[0]
-        msg = r.read()
-        print('response DELAY', msg, '\n')
-        self.a2z.write(msg)
-        self.f2a.reset()
-
-    def input_ping(self, to):
-        self.a2f.write( (to, ('ping',)) )
-
-    def getLeaks(self, fro):
-        if fro[1] == 'G_ledger':
-            print('Write to a2f:', fro, (False, ('get-leaks',)))
-            self.a2f.write( (fro, (False,('get-leaks',))) )
-        else:
-            print('Write to a2f:', fro, ('get-leaks',))
-            self.a2f.write( (fro, ('get-leaks',)) )
-        r = gevent.wait(objects=[self.f2a],count=1)
-        r = r[0]
-        msg = r.read()
-        print('response F', msg)
-        self.a2z.write( msg )
-        self.f2a.reset()
-
     def input_corrupt(self, pid):
         comm.corrupt(self.sid, pid)
 
