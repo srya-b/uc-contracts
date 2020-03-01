a27b4ce14f9f41d55d54d010318aa468a6c96323
Shreyas Gandlur <shreyasgandlur@gmail.com>
2020-03-01 17:50:32 -0600

    Modify import of UCFunctionality -> UCAsyncWrappedFunctionality

--- src/f_abb/f_offline.py
+++ src/f_abb/f_offline.py
@@ -2,13 +2,13 @@ import dump
 import gevent
 from collections import defaultdict
 
-from itm import UCFunctionality
+from itm import UCAsyncWrappedFunctionality
 
 from honeybadgermpc.field import GF
 from honeybadgermpc.elliptic_curve import Subgroup
 from honeybadgermpc.polynomial import polynomials_over
 
-class OfflinePhaseFunctionality(UCFunctionality):
+class OfflinePhaseFunctionality(UCAsyncWrappedFunctionality):
     def __init__(self, sid, pid, channel_wrapper):
         self.degree = 0 # sid needs to contain t
         self.field = GF(Subgroup.BLS12_381)
@@ -16,7 +16,7 @@ class OfflinePhaseFunctionality(UCFunctionality):
 
         self.polynomial_dict = defaultdict(lambda: self.Poly.random(degree=self.degree))
 
-        UCFunctionality.__init__(self, sid, pid, channel_wrapper)
+        UCAsyncWrappedFunctionality.__init__(self, sid, pid, channel_wrapper)
 
     def adv_msg(self, msg):
         pass
