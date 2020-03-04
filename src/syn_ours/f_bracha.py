dba7279d39a35e554bc0fb3fa9bcdce58c1b41fe
Surya Bakshi <sbaks0820@gmail.com>
2020-03-04 10:31:39 -0600

    commit changed before merge with master

--- src/syn_ours/f_bracha.py
+++ src/syn_ours/f_bracha.py
@@ -1,36 +1,43 @@
+import dump
 from itm import ITMFunctionality
 
 class Syn_Bracha_Functionality(ITMFunctionality):
     def __init__(self, sid, pid, _a2f, _f2a, _z2f, _f2z, _p2f, _f2p):
         self.ssid = sid[0]
-        self.parties = si[1]
+        self.parties = sid[1]
         self.f2p = _f2p; self.p2f = _p2f
         self.f2a = _f2a; self.a2f = _a2f
         self.f2z = _f2z; self.z2f = _z2f
 
         channels = [_a2f, _z2f, _p2f]
         handlers = {
-            _a2f: self.adversary_msg,
+            _a2f: self.adv_msg,
             _p2f: self.party_msg,
-            _z2f: self.z_msg,
+            _z2f: self.env_msg,
         }
         ITMFunctionality.__init__(self, sid, pid, channels, handlers)
 
     
-    def send_in_o1(self, *args):
+    def exec_in_o1(self, *args):
         raise Exception("'send_in_o1' unimpemented. A wrapper must derive this functionality and implement this function.")
 
-    def send_in_oD(self, delta, *args):
+    def exec_in_oD(self, delta, *args):
         raise Exception("'send_in_oD' unimpemented. A wrapper must derive this functionality and implement this function.")
 
+    def send_output(self, to, msg):
+        self.f2p.write( (to, msg) )
 
     '''Dealer, assumed to be pid=1 gives some input and invokes
     the synchronous wrapper around it to deliver the output to all
     of the parties in O(1) time.'''
     def party_input(self, pid, inp):
-         
+        if pid == 1:
+            for p in self.parties:
+                self.exec_in_o1(self.send_output, (p,))
+        dump.dump() 
 
     def party_msg(self, msg):
+        print('Party msg in bracha')
         sender,msg = msg
         sid,pid = sender
         if msg[0] == 'input':
@@ -39,8 +46,8 @@ class Syn_Bracha_Functionality(ITMFunctionality):
             self.party_output(pid)
         else: dump.dump()
 
-    def adversary_msg(self, msg):
+    def adv_msg(self, msg):
         dump.dump()
-    def z_msg(self, msg):
+    def env_msg(self, msg):
         dump.dump()
 
