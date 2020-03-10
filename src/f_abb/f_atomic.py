33cd410fb2b35826c1a99be0232efc54fe6b0dcb
Shreyas Gandlur <shreyasgandlur@gmail.com>
2020-03-09 19:42:28 -0500

    Create AtomicBroadcastFunctionality

---
+++ src/f_abb/f_atomic.py
@@ -0,0 +1,34 @@
+import dump
+import gevent
+
+from itm import UCAsyncWrappedFunctionality
+
+class AtomicBroadcastFunctionality(UCAsyncWrappedFunctionality):
+    def __init__(self, sid, pid, channels):
+        self.sid = sid
+        self.parties = None # TODO: define sid to include parties
+        self.pid = pid
+        self.BC = []
+        UCAsyncWrappedFunctionality.__init__(self, sid, pid, channels)
+        
+    def adv_msg(self, msg):
+        pass # ignore any adversarial messages
+
+    def party_msg(self, msg):
+        dealer, msg = msg
+        # need to convert sender to [0, n-1]
+        if msg[0] == 'tx':
+            tx = msg[1]
+            self.eventually(append_tx, [tx, dealer])
+
+    def env_msg(self, msg):
+        dump.dump() # environment should not attempt to contact functionality
+
+    def wrapper_msg(self, msg):
+        pass # ignore any activation by wrapper
+        
+    def append_tx(self, tx, dealer):
+        self.BC.append((tx, dealer))
+        j = len(self.BC)
+        for party in self.parties:
+            self.eventually(lambda j: self.f2p.write( (party, ('blockchain', self.BC[1:j])) ), [j])
