5958f74e748d0ad559f6090d76482e92ba7f4208
Surya Bakshi <sbaks0820@gmail.com>
2020-04-08 00:31:18 -0500

    execuc only commit: new way to crupt parties

--- src/execuc.py
+++ src/execuc.py
@@ -1,5 +1,6 @@
 from itm import ProtocolWrapper, FunctionalityWrapper, PartyWrapper, GenChannel, WrappedFunctionalityWrapper, WrappedProtocolWrapper, WrappedPartyWrapper
 from comm import setAdversary
+from utils import z_crupt
 import gevent
 
 
@@ -52,6 +53,7 @@ def createWrappedUC(fs, ps, wrapper, prot, adv):
     z2a,a2z = GenChannel('z2a'),GenChannel('a2z')
     z2w,w2z = GenChannel('z2w'),GenChannel('w2z')
     w2a,a2w = GenChannel('w2a'),GenChannel('a2w')
+    pump = GenChannel('big ol dump')
 
     env_channels = {'f2z':f2z, 'z2f':z2f, 'p2z':p2z, 'z2p':z2p, 'a2z':a2z, 'z2a':z2a, 'w2z':w2z, 'z2w':z2w}
     func_channels = {'f2z':f2z, 'z2f':z2f, 'f2p':f2p, 'p2f':p2f, 'f2a':f2a, 'a2f':a2f, 'f2w':f2w, 'w2f':w2f}
@@ -69,39 +71,41 @@ def createWrappedUC(fs, ps, wrapper, prot, adv):
     static = GenChannel()
     
     def _exec():
-        r = gevent.wait( objects=[static], count=1)
-        r = r[0]
+        r = gevent.wait( objects=[static], count=1)[0]
         d = r.read()
         m = d.msg
         static.reset()
-        assert m[0] == 'sid'
-        print('sid', m)
-        sid = m[1]
+        sid_msg,crupt_msg = m
+        assert sid_msg[0] == 'sid'
+        print('sid', sid_msg)
+        sid = sid_msg[1]
 
-        #w = wrapper(f2w, w2f, p2w, w2p, a2w, w2a, z2w, w2z)
-        w = wrapper({'f2w':f2w, 'w2f':w2f, 'p2w':p2w, 'w2p':w2p, 'a2w':a2w, 'w2a':w2a, 'z2w':z2w, 'w2z':w2z})
+        assert crupt_msg[0] == 'crupt'
+        print('crupted', crupt_msg)
+        for _s,_p in crupt_msg[1:]:
+            z_crupt(_s,_p)
+
+        w = wrapper({'f2w':f2w, 'w2f':w2f, 'p2w':p2w, 'w2p':w2p, 'a2w':a2w, 'w2a':w2a, 'z2w':z2w, 'w2z':w2z}, pump)
         gevent.spawn(w.run)
 
-        f = WrappedFunctionalityWrapper(p2f, f2p, a2f, f2a, z2f, f2z, w2f, f2w)
+        f = WrappedFunctionalityWrapper(p2f, f2p, a2f, f2a, z2f, f2z, w2f, f2w, pump)
         for t,c in fs:
             f.newcls(t,c)
         gevent.spawn( f.run )
         if ps == WrappedPartyWrapper:
             p = WrappedPartyWrapper(z2p,p2z, f2p,p2f, a2p,p2a, w2p,p2w, prot)
         else:
-            p = WrappedProtocolWrapper(z2p,p2z, f2p,p2f, a2p,p2a, w2p,p2w, prot) 
+            p = WrappedProtocolWrapper(z2p,p2z, f2p,p2f, a2p,p2a, w2p,p2w, pump, prot) 
         gevent.spawn(p.run)
         # TODO change to wrapped adversray
-        #advitm = adv(sid, -1, z2a, a2z, p2a, a2p, a2f, f2a, a2w, w2a)
-        advitm = adv(sid, -1, adv_channels)
+        advitm = adv(sid, -1, adv_channels, pump)
         setAdversary(advitm)
         gevent.spawn(advitm.run)
 
     gevent.spawn(_exec)
-    #return env_channels,func_channels,party_channels,wrapper_channels,static
-    return channels,static
+    return channels,static,pump
 
 def execWrappedUC(env, fs, ps, wrapper, prot, adv):
-    c,static = createWrappedUC(fs, ps, wrapper, prot, adv)
-    env(static, c['z2p'], c['z2f'], c['z2a'], c['z2w'], c['a2z'], c['p2z'], c['f2z'], c['w2z'])
+    c,static,pump = createWrappedUC(fs, ps, wrapper, prot, adv)
+    env(static, c['z2p'], c['z2f'], c['z2a'], c['z2w'], c['a2z'], c['p2z'], c['f2z'], c['w2z'], pump)
 
