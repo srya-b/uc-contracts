8aa96164e93998d3f689b11f83c48536581add1b
Surya Bakshi <sbaks0820@gmail.com>
2020-06-19 17:02:02 -0500

    exeuc without the wrapper

--- src/execuc.py
+++ src/execuc.py
@@ -4,43 +4,61 @@ from utils import z_crupt
 import gevent
 from numpy.polynomial.polynomial import Polynomial
 
-def createUC(fs, pwrapper, prot, adv):
-    f2p,p2f = GenChannel(),GenChannel()
-    f2a,a2f = GenChannel(),GenChannel()
-    f2z,z2f = GenChannel(),GenChannel()
-    p2a,a2p = GenChannel(),GenChannel()
-    p2z,z2p = GenChannel(),GenChannel()
-    z2a,a2z = GenChannel(),GenChannel()
-    static = GenChannel()
+#def createUC(fs, pwrapper, prot, adv):
+def createUC(fs, ps, pwrapper, adv, poly, importargs={}):
+    f2p,p2f = GenChannel('f2p'),GenChannel('p2f')
+    f2a,a2f = GenChannel('f2a'),GenChannel('a2f')
+    f2z,z2f = GenChannel('f2z'),GenChannel('z2f')
+    p2a,a2p = GenChannel('p2a'),GenChannel('a2p')
+    p2z,z2p = GenChannel('p2z'),GenChannel('z2p')
+    z2a,a2z = GenChannel('z2a'),GenChannel('a2z')
+    pump = GenChannel('ol pump and dump')
+
+    env_channels = {'f2z':f2z, 'z2f':z2f, 'p2z':p2z, 'z2p':z2p, 'a2z':a2z, 'z2a':z2a}
+    func_channels = {'f2z':f2z, 'z2f':z2f, 'f2p':f2p, 'p2f':p2f, 'f2a':f2a, 'a2f':a2f}
+    party_channels = {'p2z':p2z, 'z2p':z2p, 'p2a':p2a, 'a2p':a2p, 'p2f':p2f, 'f2p':f2p}
+    adv_channels = {'a2z':a2z, 'z2a':z2a, 'a2f':a2f, 'f2a':f2a, 'a2p':a2p, 'p2a':p2a}
+    
+    channels = {}
+    channels.update(env_channels)
+    channels.update(func_channels)
+    channels.update(party_channels)
+    channels.update(adv_channels)
+    channels.update(wrapper_channels)
+   
+    static = GenChannel('static')
     
     def _exec():
-        r = gevent.wait( objects=[static], count=1)
-        r = r[0]
-        m = r.read()
+        r = gevent.wait( objects=[static], count=1)[0]
+        m = r.read().msg
         static.reset()
-        assert m[0] == 'sid'
-        print('sid', m)
-        sid = m[1]
+        sid_msg,crupt_msg = m
+        assert sid_msg[0] == 'sid'
+        print('sid', sid_msg)
+        sid = sid_msg[1]
 
-        f = FunctionalityWrapper(p2f, f2p, a2f, f2a, z2f, f2z)
+        assert crupt_msg[0] == 'crupt'
+        print('crupted', crupt_msg)
+        for _s,_p in crupt_msg[1:]:
+            z_crupt(_s,_p)
+
+        f = FunctionalityWrapper(p2f, f2p, a2f, f2a, z2f, f2z, pump, poly, importargs)
         for t,c in fs:
             f.newcls(t,c)
         gevent.spawn( f.run )
-        if pwrapper == PartyWrapper:
-            p = PartyWrapper(z2p,p2z, f2p,p2f, a2p,p2a, prot)
-        else:
-            p = ProtocolWrapper(z2p,p2z, f2p,p2f, a2p,p2a, prot) 
+        p = ps(z2p, p2z, f2p, p2f, a2p, p2a, pump, poly, importargs)
         gevent.spawn(p.run)
-        advitm = adv(sid, -1, z2a, a2z, p2a, a2p, a2f, f2a)
+        # TODO change to wrapped adversray
+        advitm = adv(sid, -1, adv_channels, pump, poly, importargs)
         setAdversary(advitm)
         gevent.spawn(advitm.run)
     
     gevent.spawn(_exec)
-    return f2p,p2f,f2a,a2f,f2z,z2f,p2a,a2p,p2z,z2p,z2a,a2z,static
+    return channels,static,pump
 
 def execUC(env, fs, pwrapper, prot, adv):
-    f2p,p2f,f2a,a2f,f2z,z2f,p2a,a2p,p2z,z2p,z2a,a2z,static = createUC(fs, pwrapper, prot, adv)
-    env(static, z2p, z2f, z2a, a2z, p2z, f2z)
+    c,static,pump = createUC(fs, ps, adv, poly)
+    return env(static, c['z2p'], c['z2f'], c['z2a'], c['a2z'], c['p2z'], c['f2z'], pump)
 
 #def createWrappedUC(fs, ps, wrapper, prot, adv, poly):
 def createWrappedUC(fs, ps, wrapper, adv, poly, importargs={}):
@@ -92,12 +110,6 @@ def createWrappedUC(fs, ps, wrapper, adv, poly, importargs={}):
         for t,c in fs:
             f.newcls(t,c)
         gevent.spawn( f.run )
-        #if ps == WrappedPartyWrapper:
-        #    #p = WrappedPartyWrapper(z2p,p2z, f2p,p2f, a2p,p2a, w2p,p2w, pump, prot, poly)
-        #    p = WrappedPartyWrapper(z2p,p2z, f2p,p2f, a2p,p2a, w2p,p2w, pump, poly)
-        #else:
-        #    #p = WrappedProtocolWrapper(z2p,p2z, f2p,p2f, a2p,p2a, w2p,p2w, pump, prot, poly)
-        #    p = WrappedProtocolWrapper(z2p,p2z, f2p,p2f, a2p,p2a, w2p,p2w, pump, poly)
         p = ps(z2p, p2z, f2p, p2f, a2p, p2a, w2p, p2w, pump, poly, importargs)
         gevent.spawn(p.run)
         # TODO change to wrapped adversray
