54d0f1f25c21ab82493772bb4205a4b06892ece1
Surya Bakshi <sbaks0820@gmail.com>
2020-07-02 21:44:12 -0500

    new images

--- src/syn_ours/env1.py
+++ src/syn_ours/env1.py
@@ -1,9 +1,3 @@
-from itm import ProtocolWrapper, WrappedProtocolWrapper, WrappedPartyWrapper, wrappedPartyWrapper, wrappedProtocolWrapper
-from adversary import DummyWrappedAdversary
-from syn_ours import Syn_FWrapper, Syn_Channel, Syn_Bracha_Protocol
-from syn_ours.f_bracha import RBC_Simulator, Syn_Bracha_Functionality, brachaSimulator
-#from execuc import execWrappedUC
-from execuc import execWrappedUC
 from utils import z_get_leaks, waits
 import logging
 import gevent
@@ -15,7 +9,6 @@ logging.basicConfig( level=50 )
 def env1(static, z2p, z2f, z2a, z2w, a2z, p2z, f2z, w2z, pump):
     delta = 3
     n = 3
-    #sid = ('one', (1,2,3), delta)
     sid = ('one', tuple(range(1,n+1)), delta)
     static.write( (('sid', sid), ('crupt',)) )
 
@@ -34,309 +27,88 @@ def env1(static, z2p, z2f, z2a, z2w, a2z, p2z, f2z, w2z, pump):
 
     g1 = gevent.spawn(_a2z)
     g2 = gevent.spawn(_p2z)
-
-    def t(s):
-        transcript.append('cmd: ' + str(s))
-
-    z2p.write( ((sid,1), ('input', 2)), n*(4*n + 1))
-    #wait_for(p2z)
-    #waits(pump, a2z, p2z)
-    waits(pump)
-
+    
     def channel_id(fro, to, r):
         s = ('one', (sid,fro), (sid,to), r, delta)
         return (s,'F_chan')
 
-    z2a.write( ('A2W', ('get-leaks',)))
-    #msgs = waits(pump, a2z, p2z)
-    #log.debug('\033[91m [Leaks] \033[0m {}'.format( '\n'.join(str(m) for m in msgs.msg)))
+    z2p.write( ((sid,1), ('input', 2)), n*(4*n + 1))
     waits(pump)
 
-    #log.debug('\033[91m send first VAL, get +2 ECHO messages \033[0m')
+    z2a.write( ('A2W', ('get-leaks',)))
+    waits(pump)
+
+    log.debug('\033[91m send first VAL, get +2 ECHO messages \033[0m')
     for _ in range(4):
         z2w.write( ('poll',), 1 )
         waits(pump)
 
-    #log.debug('\033[91m +2 ECHO + 1 = 3 polls to send next VAL message +2 ECHO msgs \033[0m')
-    for _ in range(3):
-        z2w.write( ('poll',), 1)
-        waits(pump)
-
-    #log.debug('\033[91m +2 ECHO + 1 = 3 polls to send last VAL message +2 ECHO msgs \033[0m')
-    for _ in range(3):
-        z2w.write( ('poll',), 1)
-        waits(pump)
-
-    #log.debug('\033[91m +2 ECHO +1 = 3 polls to send 1 -> 2 ECHO msg, +2 READY msgs \033[0m')
-    for _ in range(1):
-        z2w.write( ('poll',), 1)
-        waits(pump)
-
-    #log.debug('\033[91m +2 READY +1 = 3 polls to send 1 -> 3 ECHO msg, +2 READY msgs \033[0m')
-    for _ in range(3):
-        z2w.write( ('poll',), 1)
-        waits(pump)
-
-#    log.debug('\033[91m +2 READY +1 = 3 polls to send 2 -> 1 ECHO msg, +2 READY msgs \033[0m')
-    for _ in range(3):
-        z2w.write( ('poll',), 1)
-        waits(pump)
-
-#    log.debug('\033[91m +2 READY +1 = 3 polls to send 2 -> 3 ECHO msg, +0 READY msgs \033[0m')
-    for _ in range(3):
-        z2w.write( ('poll',), 1)
-        waits(pump)
-
-#    log.debug('\033[91m DELAYING \033[0m')
-    z2a.write( ('A2W', ('delay', 3)), 3)
-    waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 1 ECHO msg, +0 msgs \033[0m')
-    for _ in range(4):
-        z2w.write( ('poll',), 1)
-        waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 2 ECHO msg, +0 msgs \033[0m')
-    z2w.write( ('poll',), 1)
-    waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 2 -> 1 READY msg, +0 msgs \033[0m')
-    z2w.write( ('poll',), 1)
-    waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 2 -> 3 READY msg, +0 msgs \033[0m')
-    z2w.write( ('poll',), 1)
-    waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 1 READY msg, 1 ACCEPTS\033[0m')
-    z2w.write( ('poll',), 1)
-    waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 2 READY msg, 2 Doesnt accept \033[0m')
-    z2w.write( ('poll',), 1)
-    waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 1 -> 2 READY msg, 2 ACCEPTS\033[0m')
-    z2w.write( ('poll',), 1)
-    waits(pump)
-
-#    log.debug('\033[91m +0 READY +1 = 3 polls to send 1 -> 3 READY msg, 3 ACCEPTS\033[0m')
-    z2w.write( ('poll',), 1)
-    waits(pump)
-
-    gevent.kill(g1)
-    gevent.kill(g2)
-
-    print('Transcript', transcript)
-    return transcript
-
-
-def env3(static, z2p, z2f, z2a, z2w, a2z, p2z, f2z, w2z, pump):
-    delta = 3
-    n = 3
-    #sid = ('one', (1,2,3), delta)
-    sid = ('one', tuple(range(1,n+1)), delta)
-    #static.write( (('sid', sid), ('crupt',)) )
-    static.write( (('sid', sid), ('crupt',(sid,2))) )
-
-    z2p.write( ((sid,1), ('input', 2)), n*(4*n + 1) )
-    #wait_for(p2z)
-    waits(pump, a2z, p2z)
-
-    def channel_id(fro, to, r):
-        s = ('one', (sid,fro), (sid,to), r, delta)
-        return (s,'F_chan')
-
-    z2a.write( ('A2W', ('get-leaks',)) )
-    msgs = waits(pump, a2z, p2z)
-    log.info('\033[91m [Leaks] \033[0m {}'.format('\n'.join(str(m) for m in msgs.msg)))
-
-    log.debug('\033[91m send first VAL, get +2 ECHO messages \033[0m')
-    for _ in range(4):
-        z2w.write( ('poll',) )
-        log.info(waits(pump, a2z, p2z))
-
     log.debug('\033[91m +2 ECHO + 1 = 3 polls to send next VAL message +2 ECHO msgs \033[0m')
-    for _ in range(4):
-        z2w.write( ('poll',) )
-        log.info(waits(pump, a2z, p2z))
-    log.debug('*****')
-    
-    z2a.write( ('A2P', ((sid,2), ('P2F', ((channel_id(2,1,1)), ('send', ('ECHO', 2)))))) )
-    log.debug('crupt output: {}'.format( waits(pump, a2z)))
-
-    z2a.write( ('A2P', ((sid,2), ('P2F', ((channel_id(2,3,1)), ('send', ('ECHO', 2)))))) )
-    log.debug('crupt output: {}'.format(waits(pump, a2z)))
+    for _ in range(3):
+        z2w.write( ('poll',), 1)
+        waits(pump)
 
     log.debug('\033[91m +2 ECHO + 1 = 3 polls to send last VAL message +2 ECHO msgs \033[0m')
     for _ in range(3):
-        z2w.write( ('poll',) )
-        log.info(waits(pump, a2z, p2z))
+        z2w.write( ('poll',), 1)
+        waits(pump)
 
     log.debug('\033[91m +2 ECHO +1 = 3 polls to send 1 -> 2 ECHO msg, +2 READY msgs \033[0m')
-    for _ in range(3):
-        z2w.write( ('poll',) )
-        log.info(waits(pump, a2z, p2z))
-
-    z2a.write( ('A2P', ((sid,2), ('P2F', ((channel_id(2,1,1)), ('send', ('READY', 2)))))) )
-    log.info('crupt output: {}'.format(waits(pump, a2z)))
-
-    z2a.write( ('A2P', ((sid,2), ('P2F', ((channel_id(2,3,1)), ('send', ('READY', 2)))))) )
-    log.info('crupt output: {}'.format(waits(pump, a2z)))
-    
-    z2a.write( ('A2P', ((sid,2), ('P2W', ('clock-round',)))) )
-    log.info('clock round: {}'.format(waits(pump, a2z)))
+    for _ in range(1):
+        z2w.write( ('poll',), 1)
+        waits(pump)
 
     log.debug('\033[91m +2 READY +1 = 3 polls to send 1 -> 3 ECHO msg, +2 READY msgs \033[0m')
     for _ in range(3):
-        z2w.write( ('poll',) )
-        log.info(waits(pump, a2z, p2z))
+        z2w.write( ('poll',), 1)
+        waits(pump)
 
     log.debug('\033[91m +2 READY +1 = 3 polls to send 2 -> 1 ECHO msg, +2 READY msgs \033[0m')
     for _ in range(3):
-        z2w.write( ('poll',) )
-        log.info(waits(pump, a2z, p2z))
+        z2w.write( ('poll',), 1)
+        waits(pump)
 
     log.debug('\033[91m +2 READY +1 = 3 polls to send 2 -> 3 ECHO msg, +0 READY msgs \033[0m')
     for _ in range(3):
-        z2w.write( ('poll',) )
-        log.info(waits(pump, a2z, p2z))
+        z2w.write( ('poll',), 1)
+        waits(pump)
+
+    log.debug('\033[91m DELAYING \033[0m')
+    z2a.write( ('A2W', ('delay', 3)), 3)
+    waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 1 ECHO msg, +0 msgs \033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
+    for _ in range(4):
+        z2w.write( ('poll',), 1)
+        waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 2 ECHO msg, +0 msgs \033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
-
-    z2a.write( ('A2W', ('get-leaks',)) )
-    msgs = waits(pump, a2z, p2z)
-    log.info('\033[91m [Leaks] \033[0m {}'.format('\n'.join(str(m) for m in msgs.msg)))
+    z2w.write( ('poll',), 1)
+    waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 2 -> 1 READY msg, +0 msgs \033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
+    z2w.write( ('poll',), 1)
+    waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 2 -> 3 READY msg, +0 msgs \033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
+    z2w.write( ('poll',), 1)
+    waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 1 READY msg, 1 ACCEPTS\033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
-    
-    z2a.write( ('A2W', ('get-leaks',)) )
-    msgs = waits(pump, a2z, p2z)
-    log.info('\033[91m [Leaks] \033[0m {}'.format('\n'.join(str(m) for m in msgs.msg)))
+    z2w.write( ('poll',), 1)
+    waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 3 -> 2 READY msg, 2 Doesnt accept \033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
+    z2w.write( ('poll',), 1)
+    waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 1 -> 2 READY msg, 2 ACCEPTS\033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
+    z2w.write( ('poll',), 1)
+    waits(pump)
 
     log.debug('\033[91m +0 READY +1 = 3 polls to send 1 -> 3 READY msg, 3 ACCEPTS\033[0m')
-    z2w.write( ('poll',) )
-    log.info(waits(pump, a2z, p2z))
-
-def env2(static, z2p, z2f, z2a, z2w, a2z, p2z, f2z, w2z, pump):
-    delta = 3
-    n = 4
-    #sid = ('one', (1,2,3), delta)
-    sid = ('one', tuple(range(1,n+1)), delta)
-    static.write( (('sid', sid), ('crupt',(sid,1))) )
-
-#    z2p.write( ((sid,1), ('input', 2)), n*(4*n + 1) )
-#    #wait_for(p2z)
-#    waits(pump, a2z, p2z)
-
-    transcript = []
-    def _a2z():
-        while True:
-            m = waits(a2z)
-            transcript.append('a2z:{}'.format(m.msg))
-            pump.write('dump')
-
-    def _p2z():
-        while True:
-            m = waits(p2z)
-            transcript.append('p2z:{}'.format(m.msg))
-            pump.write('dump')
-
-    g1 = gevent.spawn(_a2z)
-    g2 = gevent.spawn(_p2z)
-
-    def channel_id(fro, to, r):
-        s = ('one', (sid,fro), (sid,to), r, delta)
-        return (s,'F_chan')
-
-    z2a.write( ('A2P', ((sid,1), ('P2F', ((channel_id(1,2,1)), ('send', ('VAL', 3)))))), 4*n)
-    #waits(pump, a2z)
+    z2w.write( ('poll',), 1)
     waits(pump)
-
-    z2a.write( ('A2P', ((sid,1), ('P2F', ((channel_id(1,4,1)), ('send', ('VAL', 3)))))), 4*n)
-    #waits(pump, a2z)
-    waits(pump)
-    
-    z2a.write( ('A2P', ((sid,1), ('P2F', ((channel_id(1,3,1)), ('send', ('VAL', 3)))))), 4*n)
-    #waits(pump, a2z)
-    waits(pump)
-
-    z2a.write( ('A2W', ('get-leaks',)) )
-    #msgs = waits(pump, a2z, p2z)
-    #log.debug('\033[91m [Leaks] \033[0m {}'.format( '\n'.join(str(m) for m in msgs.msg)))
-    waits(pump)
-
-    # execute one of the VALs
-    z2a.write( ('A2W', ('exec', 4, 1)))
-    #waits(pump, a2z, p2z)
-    waits(pump)
-
-    # excute a VAL
-    z2a.write( ('A2W', ('exec', 4, 0)))
-    #waits(pump, a2z, p2z)
-    waits(pump)
-
-    # force execute the first ECHO
-    for _ in range(10):
-        z2w.write( ('poll',), 1)
-        #waits(pump, a2z, p2z)
-        waits(pump)
-
-    for _ in range(1):
-        # execute ECHO messages from 2->1, 2->3, 2->4
-        z2a.write( ('A2W', ('exec', 4, 0)))
-        #waits(pump, a2z, p2z)
-        waits(pump)
-
-#    for _ in range(4):
-#        z2w.write( ('poll',) )
-#        #waits(pump, a2z, p2z)
-#        waits(pump)
-#        
-#    for _ in range(2):
-#        # execute ECHO messages from 2->1, 2->3, 2->4
-#        z2a.write( ('A2W', ('exec', 4, 0)))
-#        #waits(pump, a2z, p2z)
-#        waits(pump)
-#
-#    for _ in range(9):
-#        # execute ECHO messages from 2->1, 2->3, 2->4
-#        z2a.write( ('A2W', ('exec', 7, 0)))
-#        #print(waits(pump, a2z, p2z))
-#        waits(pump)
-#
-#    for _ in range(1):
-#        z2a.write( ('A2W', ('exec', 7, 0)))
-#        #print(waits(pump, a2z, p2z))
-#        waits(pump)
-#
-#    z2a.write( ('A2W', ('exec', 7, 0)))
-#    #print('someoutput', waits(pump, a2z, p2z))
-#    waits(pump)
 
     gevent.kill(g1)
     gevent.kill(g2)
@@ -356,6 +128,12 @@ def distinguisher(t_ideal, t_real):
         print("\033[92m[Distinguisher] They're the same\033[0m")
     else:
         print("\033[91m[Distinguisher] They're different\033[0m")
+
+from itm import ProtocolWrapper, WrappedProtocolWrapper, WrappedPartyWrapper, wrappedPartyWrapper, wrappedProtocolWrapper
+from adversary import DummyWrappedAdversary
+from syn_ours import Syn_FWrapper, Syn_Channel, Syn_Bracha_Protocol
+from syn_ours.f_bracha import RBC_Simulator, Syn_Bracha_Functionality, brachaSimulator
+from execuc import execWrappedUC
 
 if __name__=='__main__':
     print('\n\t\t\033[93m [IDEAL WORLD] \033[0m\n')
