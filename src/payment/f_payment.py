40e396f9e5b9e6f57ac8c754aa0de2c5a2c5bd9d
orbxball <junyouliu9@gmail.com>
2020-08-31 09:37:56 -0400

    add action: withdraw

--- src/payment/f_payment.py
+++ src/payment/f_payment.py
@@ -110,6 +110,25 @@ class Syn_Payment_Functionality(UCWrappedFunctionality):
             leaked_msg = 'leaked msg (i don know what is the format of leaked message is look like, so a placeholder here)'
             self.leak('f2a', leaked_msg, 0)
 
+    def withdraw(self, _from, amount):
+        if self.isOpen:
+            if amount > self.balances[_from]
+                return
+
+            delay = 0 # some delay of time
+            codeblock = (
+                'schedule'
+                self.__withdraw,
+                (_from, amount),
+                delay
+            )
+            self.write('f2w', codeblock)
+            m = wait_for(self.channels['w2f']).msg
+            assert m == ('OK',)
+
+            leaked_msg = 'leaked msg (i don know what is the format of leaked message is look like, so a placeholder here)'
+            self.leak('f2a', leaked_msg, 0)
+
     # the handler bound on p2f channel, handling message from parties
     def party_msg(sef, msg):
         log.debug('Party message in payment {}'.format(msg))
@@ -135,6 +154,10 @@ class Syn_Payment_Functionality(UCWrappedFunctionality):
             sender = data['sender']
             amount = data['amount']
             deposit(sender, amount)
+        elif command == 'withdraw':
+            sender = data['sender']
+            amount = data['amount']
+            withdraw(sender, amount)
         else:
             self.pump.write("pump")
 
