251fc9bbc0f2c0f4aad3d10ee91f936a5a8c6bb4
Surya Bakshi <sbaks0820@gmail.com>
2020-09-09 20:23:29 -0500

    trimmed comm, utils and added crupt

--- src/comm.py
+++ src/comm.py
@@ -27,64 +27,64 @@ PARTY = 1
 ADVERSARY = 2
 SIMULATOR = 3
 
-def cset(sid,pid,tag,itm):
-    if (sid,pid) not in commap:
-        commap[sid,pid] = tag
-        itmmap[sid,pid] = itm
-def cset2(sid,pid,tag):
-    if (sid,pid) not in commap:
-        commap[sid,pid] = tag
-
-def isf(sid,pid):
-    try:
-        return commap[sid,pid] == FUNCTIONALITY
-    except KeyError:
-        return False
-
-def isadversary(sid,pid):
-    try:
-        return commap[sid,pid] == ADVERSARY
-    except KeyError:
-        return False
-
-def isparty(sid,pid):
-    try:
-        return commap[sid,pid] == PARTY
-    except KeyError:
-        return False
-
-def getitm(sid,pid):
-    try:
-        return itmmap[sid,pid]
-    except KeyError:
-        return None
-
-def itmstring(sid,pid):
-    try:
-        return str(getitm(sid,pid))
-    except:
-        return ''
-
-def setAdversary(itm):
-    global adversary
-    sid,pid = itm.sid,itm.pid
-    cset(sid,pid,ADVERSARY,itm)
-    adversary = itm
-
-def setFunctionality(itm):
-    sid,pid = itm.sid,itm.pid
-    cset(sid,pid,FUNCTIONALITY,itm)
-
-def setFunctionality2(sid,pid):
-    cset2(sid,pid,FUNCTIONALITY)
-
-def setParty(itm):
-    sid,pid = itm.sid,itm.pid
-    cset(sid,pid,PARTY,itm)
-
-def setParties(itms):
-    for p in itms:
-        setParty(p)
+#def cset(sid,pid,tag,itm):
+#    if (sid,pid) not in commap:
+#        commap[sid,pid] = tag
+#        itmmap[sid,pid] = itm
+#def cset2(sid,pid,tag):
+#    if (sid,pid) not in commap:
+#        commap[sid,pid] = tag
+#
+#def isf(sid,pid):
+#    try:
+#        return commap[sid,pid] == FUNCTIONALITY
+#    except KeyError:
+#        return False
+#
+#def isadversary(sid,pid):
+#    try:
+#        return commap[sid,pid] == ADVERSARY
+#    except KeyError:
+#        return False
+#
+#def isparty(sid,pid):
+#    try:
+#        return commap[sid,pid] == PARTY
+#    except KeyError:
+#        return False
+#
+#def getitm(sid,pid):
+#    try:
+#        return itmmap[sid,pid]
+#    except KeyError:
+#        return None
+#
+#def itmstring(sid,pid):
+#    try:
+#        return str(getitm(sid,pid))
+#    except:
+#        return ''
+#
+#def setAdversary(itm):
+#    global adversary
+#    sid,pid = itm.sid,itm.pid
+#    cset(sid,pid,ADVERSARY,itm)
+#    adversary = itm
+#
+#def setFunctionality(itm):
+#    sid,pid = itm.sid,itm.pid
+#    cset(sid,pid,FUNCTIONALITY,itm)
+#
+#def setFunctionality2(sid,pid):
+#    cset2(sid,pid,FUNCTIONALITY)
+#
+#def setParty(itm):
+#    sid,pid = itm.sid,itm.pid
+#    cset(sid,pid,PARTY,itm)
+#
+#def setParties(itms):
+#    for p in itms:
+#        setParty(p)
 
 def corrupt(sid,pid):
     global corrupted
