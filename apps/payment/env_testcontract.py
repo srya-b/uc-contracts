f7f9531aa66ebfc241b6fbc81ffb5605b490900d
orbxball <junyouliu9@gmail.com>
2020-11-13 10:30:29 -0500

    fix forwarding msg in offchain channel

--- apps/payment/env_testcontract.py
+++ apps/payment/env_testcontract.py
@@ -24,20 +24,23 @@ def env1(k, static, z2p, z2f, z2a, z2w, a2z, p2z, f2z, w2z, pump):
     g1 = gevent.spawn(_a2z)
     g2 = gevent.spawn(_p2z)
 
-    z2p.write( ((sid,1), ('close', (6, 14, 1), 'P_S sig')) )
+    z2p.write( ((sid,1), ('send', 2, ('pay', (8, 12, 1), 'P_s sig'), 0)) )
     waits(pump)
 
-    z2a.write( ('A2W', ('exec', 4, 0), 0) )
+    z2p.write( ((sid,1), ('close', (6, 14, 2), 'P_s sig')) )
     waits(pump)
+
+    # z2a.write( ('A2W', ('exec', 4, 0), 0) )
+    # waits(pump)
 
     for _ in range(5):
         z2w.write( ('poll',), 1 )
         waits(pump)
 
-    z2p.write( ((sid,2), ('challenge', (7, 13, 2), 'P_r sig')) )
+    z2p.write( ((sid,2), ('challenge', (7, 13, 3), 'P_r sig')) )
     waits(pump)
 
-    for _ in range(7):
+    for _ in range(10):
         z2w.write( ('poll',), 1 )
         waits(pump)
 
