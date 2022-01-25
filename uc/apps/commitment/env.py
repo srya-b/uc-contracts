4fb0e4ee557edcea0faf251e6144e901ec52fc7f
Surya Bakshi <surya.bakshi@protonmail.com>
2022-01-25 11:43:02 -0600

    commented the commitment environment

--- uc/apps/commitment/env.py
+++ uc/apps/commitment/env.py
@@ -1,11 +1,39 @@
 from uc.utils import waits, collectOutputs
 import gevent
 
+
+""" This files contains several environments that are run by execUC a the 
+bottom of the files. All environments conform to some dedault behavior:
+  1. Setting the SID of this session. The SID often encodes protocol parameters.
+  2. The list of crupt parties. This is the static corruptions model.
+  3. Return some transcript of the communication from the honest parties and the
+     adversary.
+"""
+
 def env(k, static, z2p, z2f, z2a, a2z, f2z, p2z, pump):
     print('\033[94m[ env_honest ]\033[0m')
+
+    # The SID is encoded, ideally, as a tupe of strings. In reality
+    # you can encode it however you want as long as the protocols/functionalities
+    # you are running know how to parse it for information. The string is the
+    # easiest as it works well with the multisession extension. ITMs then parse
+    # the SID using `literal_eval` from the `ast` python package. In this example
+    # the SID encodes some string to identify the session and the two PIDs of the
+    # parties in this protocol: 1 and 2. 
+    # See prot_com and F_com for how the SID is parsed.
     sid = ('one', "1, 2")
+
+    # The static channel is given by `execUC` which waits to read the sid
+    # and the set of crupt parties. In this case there are no crupt parties
+    # hence nothing after `crupt`.
     static.write( (('sid',sid), ('crupt',)))
 
+    # these two functions simply wait to read on the p2z and a2z channels
+    # of the environment and append the message to a transcript that is returned
+    # at the end of the environment code. Often times, when there are corrupt
+    # parties you'll want to manually read from the a2z channel or p2z channel
+    # and use the information in a meaningful way. (See other environments in this
+    # and other apps.
     transcript = []
     def _a2z():
         while True:
@@ -19,26 +47,35 @@ def env(k, static, z2p, z2f, z2a, a2z, f2z, p2z, pump):
             transcript.append('p2z: ' + str(m))
             pump.write('dump')
 
+    # spawn the functions
     g1 = gevent.spawn(_a2z)
     g2 = gevent.spawn(_p2z)
     
 
+    # Give input msg=('commt', 0) to paty with PID=1 
     z2p.write( (1, ('commit',0)))
     waits(pump)
+    # the `pump` channel is given to ALL ITMs and it is used for them to
+    # forfeit control back to the environment. An ITM may either write 
+    # to another ITM or return control to the environment through `pump`.
 
     z2p.write( (1, ('reveal',)))
     waits(pump)
-    
+   
+    # kill the two threads
     gevent.kill(g1)
     gevent.kill(g2)
 
+    # return the transcript
     print('transcript', transcript)
     return transcript
 
 def env_receiver_crupt(k, static, z2p, z2f, z2a, a2z, f2z, p2z, pump):
     print('\033[94m[ env_receiver_crupt ]\033[0m')
     sid = ('one', "1, 2")
-    static.write( (('sid',sid), ('crupt', (sid,2))))
+
+    # same SID but now PID=2 is crupt
+    static.write( (('sid',sid), ('crupt', 2)))
 
     transcript = []
     def _p2z():
@@ -70,6 +107,7 @@ def env_receiver_crupt(k, static, z2p, z2f, z2a, a2z, f2z, p2z, pump):
 def env_committer_crupt(k, static, z2p, z2f, z2a, a2z, f2z, p2z, pump):
     print('\033[94m[ env_committer_crupt ]\033[0m')
     sid = ('one', "1, 2")
+
     static.write( (('sid',sid), ('crupt', 1)))
 
     transcript = []
@@ -83,11 +121,18 @@ def env_committer_crupt(k, static, z2p, z2f, z2a, a2z, f2z, p2z, pump):
 
     gevent.spawn(_p2z)
 
+
+    # Notice here we don't have any function to read from a2z 
+    # automatically because we want to use the information on it
+    # in a meaningful way.
     z2a.write( ('A2F', ('hash', (123, 0))))
     m = waits(a2z)
     print('env msg', m)
     _,lasthash = m
     print('last hash', lasthash)
+
+    # we still (optionally) choose to append the message to the
+    # transcript
     transcript.append('a2z: ' + str(m))
 
     z2a.write( ('A2P', (1, ('sendmsg', 2, ('commit',lasthash)))))
@@ -134,12 +179,19 @@ def env_committer_crupt_bad_open(k, static, z2p, z2f, z2a, a2z, f2a, p2z, pump):
     z2a.write( ('A2P', (1, ('sendmsg', 2, 'yoyoyo'))) )
     waits(pump)
 
+    # Committer commits, but opens with the wrong nonce, receiver
+    # should not accept 
     z2a.write( ('A2P', (1, ('sendmsg', 2, ('open', (123, 1))))))
     waits(pump)
 
     return transcript
     
 
+# Distinguisher might be a bad name here, this just prints the 
+# transcripts in a pretty way and compares them with direct 
+# equality. In reality many times with protocols that sample
+# random information like the pedersen commitment, the transcripts
+# ay never be equal.
 def distinguisher(t_ideal, t_real):
     print('\n\t\033[93m Ideal transcript\033[0m')
     for i in t_ideal: print(str(i))
@@ -161,6 +213,8 @@ from sim_com import Sim_Com
 from uc.protocol import DummyParty, protocolWrapper
 
 if __name__=='__main__':
+    # run the ideal execution first with the dummy parties
+    # and the simulator that we created
     tideal = execUC(
         128,
         env_receiver_crupt,
@@ -170,6 +224,8 @@ if __name__=='__main__':
     )
 
     print('\n')
+    # the real world uses the dummy adversary and F_ro + Channel
+    # as the hybrid functionality that prot_com uses.
     treal = execUC(
         128,
         env_receiver_crupt,
