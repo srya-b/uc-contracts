e83ccc9b4b9b64129a00ca1ab63ffd161d994eef
Surya Bakshi <surya.bakshi@protonmail.com>
2022-02-02 11:51:35 -0600

    missing mcom files

---
+++ uc/apps/pedersen/env_bangfcom.py
@@ -0,0 +1,85 @@
+from uc.utils import waits, collectOutputs
+import os
+import gevent
+import secp256k1 as secp
+
+def env_honest(k, static, z2p, z2f, z2a, a2z, f2z, p2z, pump):
+    print('\033[94m[ env_honest ]\033[0m')
+
+    sid = ('one', "1, 2")
+    static.write( (('sid',sid), ('crupt',)))
+
+    transcript = []
+    def _a2z():
+        while True:
+            m = waits(a2z)
+            transcript.append('a2z: ' + str(m))
+            pump.write('dump')
+
+    def _p2z():
+        while True:
+            m = waits(p2z)
+            transcript.append('p2z: ' + str(m))
+            pump.write('dump')
+
+    g1 = gevent.spawn(_a2z)
+    g2 = gevent.spawn(_p2z)
+
+    for i in range(2):
+        m = secp.uint256_from_str(os.urandom(32))
+        print('\n commiting to the point: \n\t{}\n'.format(m))
+
+        z2p.write( (1, ((str(i), "1,2"), ('commit', i, m))) )
+        waits(pump)
+
+    for i in range(2):
+        z2p.write( (1, ((str(i), "1,2"),('reveal', i))) )
+        waits(pump)
+    
+    gevent.kill(g1)
+    gevent.kill(g2)
+    
+    print('transcript', transcript)
+    return transcript
+
+def distinguisher(t_ideal, t_real):
+    print('\n\t\033[93m ideal transcript\033[0m')
+    for i in t_ideal: print(str(i))
+
+    print('\n\t\033[93m real transcript\033[0m')
+    for i in t_real: print(str(i))
+
+    if t_ideal == t_real:
+        print("\033[92m[distinguisher] they're the same\033[0m")
+    else:
+        print("\033[91m[distinguisher] they're different\033[0m")
+
+
+from uc.adversary import DummyAdversary
+from uc.protocol import DummyParty
+from uc.execuc import execUC
+from f_mcrs import F_MCRS
+from uc.apps.commitment import F_Com_Channel
+from uc.multisession import bangP, bangF
+from prot_com import Commitment_Prot
+
+print('\nreal\n')
+treal = execUC(
+    128,
+    env_honest,
+    F_MCRS,
+    bangP(Commitment_Prot),
+    DummyAdversary
+)
+
+print('\nideal\n')
+tideal = execUC(
+    128,
+    env_honest,
+    #F_Mcom,
+    bangF(F_Com_Channel),
+    DummyParty,
+    DummyAdversary
+)
+
+distinguisher(tideal, treal)
---
+++ uc/apps/pedersen/f_mcrs.py
@@ -0,0 +1,36 @@
+from ast import literal_eval
+from uc import UCFunctionality
+from uc.utils import read_one, read
+from secp256k1 import make_random_point
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class F_MCRS(UCFunctionality):
+    def __init__(self, k, bits, crupt, sid, channels, pump):
+        UCFunctionality.__init__(self, k, bits, crupt, sid, channels, pump)
+        self.ssid,sid = sid
+
+        self.output_value = None
+        self.bit = None
+        self.state = 0 # wait to commit, 1: committed, 2: reveal
+
+        self.party_msgs['value'] = self.value
+        self.party_msgs['sendmsg'] = self.sendmsg
+
+        self.p_msg = lambda x: x[1][0]
+        self.p_parse = lambda x: (x[0], *x[1][1:])
+
+    def value(self, senderpid, senderssid):
+        if self.output_value is None:
+            g = make_random_point(lambda x: self.sample(8*x).to_bytes(x,'little') )
+            h = make_random_point(lambda x: self.sample(8*x).to_bytes(x,'little') )
+            self.output_value = (g,h)
+        self.write( 'f2p', (senderpid, (senderssid, (self.output_value,))) )
+
+    def sendmsg(self, senderpid, senderssid, to, msg):
+        self.write(
+            ch='f2p',
+            msg=(to, (senderssid, ('recvmsg', senderpid, msg)))
+        )
